2ad481c7f04bfa25baf9cd9b38a109888ba6b4a7
Lucas <lucas.dos.fernandes@accenture.com>
2020-08-02 23:55:42 -0300

    ajuste url heroku

--- src/main/java/com/lucasdsf/api/eureka/EurekaServerApplication.java
+++ src/main/java/com/lucasdsf/api/eureka/EurekaServerApplication.java
@@ -1,11 +1,13 @@
 package com.lucasdsf.api.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
