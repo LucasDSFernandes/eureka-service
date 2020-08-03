16ed2ba7ce7a7de942791b83c93e524e46d7f055
Lucas <lucas.dos.fernandes@accenture.com>
2020-08-03 01:11:34 -0300

    ajuste anotation eureka

--- src/main/java/com/lucasdsf/api/eureka/EurekaServerApplication.java
+++ src/main/java/com/lucasdsf/api/eureka/EurekaServerApplication.java
@@ -1,13 +1,11 @@
 package com.lucasdsf.api.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
